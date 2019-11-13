ac16aece4e57c872f29a151d2f90778b5ecb7774
adamovicm <adam.thor@live.com>
2019-11-13 11:37:08 +0100

    task 1 done

---
+++ src/components/AnimalList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1>List of animals</h1>
+
+    <template>
+      <div>
+        <b-table striped hover :items="animals">
+          
+        </b-table>
+      </div>
+    </template>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      animals: [
+        {
+          species: 'Lion',
+          name: 'Simba',
+          dob: '2010-02-12'
+        },
+        {
+          species: 'Tiger',
+          name: 'Shere Khan',
+          dob: '2008-12-25'
+        },
+        {
+          species: 'Panther',
+          name: 'Bagheera',
+          dob: '2015-06-02'
+        },
+        {
+          species: 'Elephant',
+          name: 'Dumbo',
+          dob: '2001-08-10'
+        },
+        {
+          species: 'Bear',
+          name: 'Baloo',
+          dob: '2010-02-12'
+        },
+      ]
+    }
+  } 
+  
+    
+}
+</script>
